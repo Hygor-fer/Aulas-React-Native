860ff7c2588a9d333e5bb7eaf91e898a7c908ed7
hygor lindo <hygor.fernandes@escola.pr.gov.br>
2023-05-26 11:31:25 -0300

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+impo*/rt { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -9,6 +9,25 @@ export default function App() {
     </View>
   );
 }
+
+//COMMIT FINAL (SO MAIS UMA VEZ ESTOU ENSINANDP )..
+
+/*COMANDOS PARA FAZER O COMMIT 
+
+  1) echo "# aula01" >> README.md 
+  2) git init
+  3) git status -u
+  4) git add .
+  5) git status -u
+  6) git commit -m "first commit"
+  7) git config user.email "email do seu usuario cadastrado do github"
+  8) git config user.name "informação adicional"
+  9) git commit -m "first commit"
+  10) git status -u
+  11) git remote add origin "link"
+  12) git push -u origin master 
+  13) git log
+  */
 
 const styles = StyleSheet.create({
   container: {
